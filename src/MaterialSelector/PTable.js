b67edf0a04b33c3af7faa9ef874f7177082e5814
Kristjan Eimre <kristjaneimre@gmail.com>
2023-01-15 19:52:22 +0200

    filtering mode help-box

--- src/MaterialSelector/PTable.js
+++ src/MaterialSelector/PTable.js
@@ -3,6 +3,10 @@ import React from "react";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
 
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+
+import Popover from "react-bootstrap/Popover";
+
 import { element_symbols } from "./ptable_data";
 
 import "./PTable.css";
@@ -12,7 +16,6 @@ class Element extends React.Component {
   // 0 - deselected
   // 1 - include
   // 2 - exclude
-  // 3 - only
 
   constructor(props) {
     super(props);
@@ -23,20 +26,21 @@ class Element extends React.Component {
 
   handleOnClick() {
     if (this.props.disabled) return;
+    if (this.props.onSelectionChange == null) return;
     this.props.onSelectionChange({ element: this.symbol });
   }
 
   render() {
     let e_class = `pt_element pt_element-${this.props.num}`;
 
-    let selection = 0;
-    if (this.symbol in this.props.filter["elements"])
-      selection = this.props.filter["elements"][this.symbol];
-
     if (this.props.disabled) {
       e_class += " pt_element-disabled";
     } else {
-      e_class += ` pt_element-state${selection}`;
+      e_class += ` pt_element-state${this.props.selection}`;
+    }
+
+    if (!this.props.doc) {
+      e_class += " nondoc";
     }
 
     if (this.props.num >= 57 && this.props.num <= 71) {
@@ -52,6 +56,58 @@ class Element extends React.Component {
   }
 }
 
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Header as="h3">Filtering mode help</Popover.Header>
+    <Popover.Body>
+      <b>Exact</b>
+      <br />
+      Only the selected elements are allowed in the resulting chemical formula.
+      For example
+      <div className="doc-elements">
+        <Element
+          num={14}
+          disabled={false}
+          onSelectionChange={null}
+          selection={1}
+          doc={true}
+        />
+        <Element
+          num={8}
+          disabled={false}
+          onSelectionChange={null}
+          selection={1}
+          doc={true}
+        />
+      </div>
+      only allows for formulas in the form of Si<sub>x</sub>O<sub>y</sub>
+      .
+      <br />
+      <b>Include/exclude</b>
+      <br />
+      The green selected elements must be included, while the red elements must
+      not be included in the formula. For example
+      <div className="doc-elements">
+        <Element
+          num={14}
+          disabled={false}
+          onSelectionChange={null}
+          selection={1}
+          doc={true}
+        />
+        <Element
+          num={8}
+          disabled={false}
+          onSelectionChange={null}
+          selection={2}
+          doc={true}
+        />
+      </div>
+      filters for materials that contain Si and any other elements except O.
+    </Popover.Body>
+  </Popover>
+);
+
 class SelectionMode extends React.Component {
   constructor(props) {
     super(props);
@@ -60,6 +116,11 @@ class SelectionMode extends React.Component {
   render() {
     return (
       <div className="selection_mode_outer">
+        <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+          <div className="help_button">
+            <span className="help_text">?</span>
+          </div>
+        </OverlayTrigger>
         <div
           className="selection_mode_inner"
           onChange={(e) =>
@@ -93,13 +154,19 @@ class PTable extends React.Component {
       if (i > 86) {
         disabled = true;
       }
+      let symbol = element_symbols[i];
+      let selection = 0;
+      if (symbol in this.props.filter["elements"])
+        selection = this.props.filter["elements"][symbol];
+
       items.push(
         <Element
           key={i}
           num={i}
           disabled={disabled}
           onSelectionChange={this.props.onSelectionChange}
-          filter={this.props.filter}
+          selection={selection}
+          doc={false}
         />
       );
     }
